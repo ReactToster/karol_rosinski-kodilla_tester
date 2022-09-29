0633db582a155af165545a38c269d4f0f3e80720
Karol Rosiński <krosinski94@gmail.com>
2022-09-29 16:02:53 +0200

    Typy Danych

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Karol");
+        System.out.println(28);
+        System.out.println('R');
+    }
+}
