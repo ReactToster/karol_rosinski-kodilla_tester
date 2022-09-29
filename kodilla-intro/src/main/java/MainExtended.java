db5eb07f2c2f045148dfc101e728bcd0f6eadf20
Karol Rosiński <krosinski94@gmail.com>
2022-09-29 16:26:08 +0200

    Zmienne

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        int userId = 66;
+        double userBestTime = 23.45;
+        String userName = "Xeno";
+        char userLetter = 'V';
+        boolean isAdult = true;
+        long userIncome = 6500033;
+        System.out.println(userId);
+        System.out.println(userBestTime);
+        System.out.println(userName);
+        System.out.println(userLetter);
+        System.out.println(isAdult);
+        System.out.println(userIncome);
+    }
+}
