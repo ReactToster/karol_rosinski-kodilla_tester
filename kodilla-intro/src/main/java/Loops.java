ddf3a6c3e3b95aa01afd012653a227d2e58e2c6e
Karol Rosiński <krosinski94@gmail.com>
2022-10-03 19:15:30 +0200

    Pętle - for

---
+++ kodilla-intro/src/main/java/Loops.java
@@ -0,0 +1,20 @@
+public class Loops {
+    public static void main(String[] args) {
+        String[] names = new String[] {"Zygfryd", "Gwidon", "Florentyna"};
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+
+        int[] superArray = new int[] {2, 56, 990, 23, 6, 1023, 543, 89};
+        int sum = sumUpElementsOfArray(superArray);
+    }
+
+    private static int sumUpElementsOfArray(int[] array){
+        int sumOfArrayElements = 0;
+        for (int i = 0; i < array.length; i++){
+            sumOfArrayElements = sumOfArrayElements + array[i];
+        }
+        return sumOfArrayElements;
+    }
+}
