33be0111e5e5e06bbc6b3db038b8a4d41dc7b395
Karol Rosiński <krosinski94@gmail.com>
2022-10-03 09:45:56 +0200

    Instrukcje Warunkow

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,5 +1,11 @@
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        if (1 > 2){
+            System.out.println("Matematykę zostawiam wam.");
+        } else {
+            System.out.println("Matematykę zostawcie mi");
+        }
     }
 }
