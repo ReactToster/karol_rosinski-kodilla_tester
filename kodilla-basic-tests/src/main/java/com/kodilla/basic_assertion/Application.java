bfebbf86ecda6540fc00dd1ded81caf8a07364c4
Karol Rosiński <krosinski94@gmail.com>
2022-10-22 16:40:19 +0200

    Zadanie: rozbudowa testów kalkulatora

--- kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
@@ -12,5 +12,21 @@ public class Application {
         } else {
             System.out.println("Metoda sum nie działa poprawnie dla liczb: " + a + " i " + b);
         }
+
+        int subtractResult = calculator.subtract(a, b);
+        correct = ResultChecker.assertEquals(-3, subtractResult);
+        if(correct) {
+            System.out.println("Metoda subtract działa poprawnie dla liczb: " + a + " i " + b);
+        } else {
+            System.out.println("Metoda subtract nie działa poprawnie dla liczb: " + a + " i " + b);
+        }
+
+        int exponentiationResult = calculator.exponentiation(a);
+        correct = ResultChecker.assertEquals(25, exponentiationResult);
+        if(correct) {
+            System.out.println("Metoda exponentiation działa poprawnie dla liczby " + a );
+        } else {
+            System.out.println("Metoda exponentiation nie działa poprawnie dla liczby " + a );
+        }
     }
 }
--- kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
@@ -3,4 +3,5 @@ package com.kodilla.basic_assertion;
 public class Calculator {
     public int add(int a, int b) {return a+b;}
     public int subtract(int a, int b) {return a-b;}
+    public int exponentiation(int a) {return a*a;}
 }
