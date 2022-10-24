a53047a97ea6b98ed5d3387accb3952b6c48e7aa
Karol Rosiński <krosinski94@gmail.com>
2022-10-24 16:41:41 +0200

    Zadanie: bankomat p1

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java
@@ -0,0 +1,29 @@
+package com.kodilla.bank.homework;
+
+public class Bank {
+    private CashMachine[] cashMachines;
+
+    public CashMachine[] getCashMachines() {
+        return cashMachines;
+    }
+
+    public double finalBalance() {
+        return -1;
+    }
+
+    public int amountOfDeposit() {
+        return -1;
+    }
+
+    public int amountOfWithdrawal() {
+        return -1;
+    }
+
+    public double averageDeposit() {
+        return -1;
+    }
+
+    public double averageWithdrawal() {
+        return -1;
+    }
+}
---
+++ kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/CashMachine.java
@@ -0,0 +1,13 @@
+package com.kodilla.bank.homework;
+
+public class CashMachine {
+    private double[] transactions;
+
+    public double balance() {
+        return -1;
+    }
+
+    public int amountOfTransactions(){
+        return -1;
+    }
+}
