20ef299ebd73753695670c2d31362a11c9268fe3
Karol Rosiński <krosinski94@gmail.com>
2022-10-27 18:22:06 +0200

    Interfejsy

---
+++ kodilla-collections/src/main/java/com/kodilla/collections/interfaces/InterfacesDemo.java
@@ -0,0 +1,19 @@
+package com.kodilla.collections.interfaces;
+
+public class InterfacesDemo {
+    public static void main(String[] args) {
+        Square square = new Square(10.0);
+        showShapeDetails(square);
+
+        Circle circle = new Circle(7);
+        showShapeDetails(circle);
+
+        Triangle triangle = new Triangle(2, 4, 8);
+        showShapeDetails(triangle);
+    }
+
+    private static void showShapeDetails(Shape shape) {
+        System.out.println(shape.getArea());
+        System.out.println(shape.getPerimeter());
+    }
+}
---
+++ kodilla-collections/src/main/java/com/kodilla/collections/interfaces/Shape.java
@@ -0,0 +1,7 @@
+package com.kodilla.collections.interfaces;
+
+public interface Shape {
+    public double getArea();
+
+    public double getPerimeter();
+}
