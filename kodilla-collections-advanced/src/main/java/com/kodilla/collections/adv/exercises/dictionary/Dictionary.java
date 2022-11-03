767f84c2420b851508ab9fb60efcff43c40d7ee6
Karol Rosiński <krosinski94@gmail.com>
2022-11-03 17:33:45 +0100

    Przykład testów z użyciem kolekcji

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/dictionary/Dictionary.java
@@ -0,0 +1,31 @@
+package com.kodilla.collections.adv.exercises.dictionary;
+
+import java.util.*;
+
+public class Dictionary {
+    Map<String, List<EnglishWord>> dictionary = new HashMap<>();
+
+    public void addWord(String polishWord, EnglishWord englishWord){
+        List<EnglishWord> englishWords = dictionary.getOrDefault(polishWord, new ArrayList<>());
+        englishWords.add(englishWord);
+        dictionary.put(polishWord, englishWords);
+    }
+
+    public List<EnglishWord> findEnglishWords(String polishWord) {
+        return dictionary.getOrDefault(polishWord, Collections.emptyList());
+    }
+
+    public List<EnglishWord> findEnglishWords(String polishWord, PartOfSpeech partOfSpeech){
+        List<EnglishWord> result = new ArrayList<>();
+        for (EnglishWord englishword :
+                dictionary.getOrDefault(polishWord, Collections.emptyList())) {
+            if (englishword.getPartOfSpeech().equals(partOfSpeech))
+                result.add(englishword);
+        }
+        return result;
+    }
+
+    public int size(){
+        return dictionary.size();
+    }
+}
