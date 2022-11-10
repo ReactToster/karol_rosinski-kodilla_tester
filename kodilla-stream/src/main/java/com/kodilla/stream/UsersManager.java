2b5328ce74abba24d3cb98477988f8a0d5b94e41
Karol Rosiński <krosinski94@gmail.com>
2022-11-10 13:17:41 +0100

    Stream - programowanie fukncyjne

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/UsersManager.java
@@ -0,0 +1,25 @@
+package com.kodilla.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UsersManager {
+    public static void main(String[] args) {
+        List<String> chemistsGroupUsernames = filterChemistsGroupUsernames();
+        System.out.println(chemistsGroupUsernames);
+    }
+
+    public static String getUserName(User user) {
+        return user.getUsername();
+    }
+
+    private static List<String> filterChemistsGroupUsernames() {
+        List<String> usernames = UserRepository.getUsersList()
+                .stream()
+                .filter(user -> user.getGroup().equals("Chemists"))
+                .map(UsersManager::getUserName)
+                .collect(Collectors.toList());
+
+        return usernames;
+    }
+}
