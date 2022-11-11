19ad83630f1ce74209503cbb67661a041f3e8e93
Karol Rosiński <krosinski94@gmail.com>
2022-11-11 12:53:33 +0100

    Zadanie: uczniowie i nauczyciele - console students and teachers

---
+++ kodilla-stream/src/main/java/com/kodilla/optional/homework/Application.java
@@ -0,0 +1,24 @@
+package com.kodilla.optional.homework;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Application {
+    public static void main(String[] args) {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Adam Karuzela", new Teacher("Tomasz Kierownik")));
+        students.add(new Student("Karol Racuchowski", null));
+        students.add(new Student("Marta Zadaniowa", new Teacher("Zbigniew Ryciński")));
+        students.add(new Student("Dominika Walecka", null));
+
+        for (Student student :
+                students) {
+            Optional<Teacher> optionalTeacher = Optional.ofNullable(student.getTeacher());
+            System.out.println(
+                    "uczeń: " + student.getName() +
+                    " nauczyciel: " + optionalTeacher.orElse(new Teacher("<undefined>")).getName()
+            );
+        }
+    }
+}
