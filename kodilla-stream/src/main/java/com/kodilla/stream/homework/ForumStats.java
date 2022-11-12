b2b49e3ce0891047432ab85aedb8231564780ee0
Karol Rosiński <krosinski94@gmail.com>
2022-11-12 18:23:09 +0100

    Zadanie: średnia liczba postów add implementation and test for calculating average number of posts

--- kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
@@ -6,21 +6,28 @@ import com.kodilla.stream.UserRepository;
 import java.util.List;
 
 public class ForumStats {
-
+    private int age = 40;
     public static void main(String[] args) {
         ForumStats forumStats = new ForumStats();
-        System.out.println("Average Number of posts for users younger than 40: " + forumStats.calculateAverageNumberOfPostsForUsersYoungerThan40(UserRepository.getUsersList()) +
+
+        System.out.println("Average Number of posts for users younger than 40: " + forumStats.calculateAverageNumberOfPostsForUsersYoungerThanGivenAge(UserRepository.getUsersList()) +
                 "\n" +
-                        "Average Number of posts for users older or equal to 40: " + forumStats.calculateAverageNumberOfPostsForUsersOlderOrEqualTo40(UserRepository.getUsersList())
+                        "Average Number of posts for users older or equal to 40: " + forumStats.calculateAverageNumberOfPostsForUsersOlderOrEqualToGivenAge(UserRepository.getUsersList())
                 );
-
     }
 
-    public double calculateAverageNumberOfPostsForUsersOlderOrEqualTo40(List<User> users) {
-        return 0.0;
+    public double calculateAverageNumberOfPostsForUsersOlderOrEqualToGivenAge(List<User> users) {
+        double averageNumberOfPosts = users
+                .stream()
+                .filter(user -> user.getAge() >= age)
+                .mapToInt(user -> user.getNumberOfPost())
+                .average()
+                .getAsDouble();
+
+        return averageNumberOfPosts;
     }
 
-    public double calculateAverageNumberOfPostsForUsersYoungerThan40(List<User> users) {
+    public double calculateAverageNumberOfPostsForUsersYoungerThanGivenAge(List<User> users) {
         return 0.0;
     }
 }
---
+++ kodilla-stream/src/test/java/com/kodilla/stream/homework/ForumStatsTest.java
@@ -0,0 +1,35 @@
+package com.kodilla.stream.homework;
+
+import com.kodilla.stream.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ForumStatsTest {
+    private ForumStats forumStats;
+    private List<User> testUsers;
+
+    @BeforeEach
+    public void setForumStatsData() {
+        forumStats = new ForumStats();
+        testUsers = new ArrayList<>();
+        testUsers.add(new User("ronald", 41, 21, "Stronghold"));
+        testUsers.add(new User("LegendarySlayer", 18, 256, "Stronghold"));
+        testUsers.add(new User("Butterfly111", 50, 51, "Castle"));
+        testUsers.add(new User("final_gambit", 30, 221, "Fortress"));
+        testUsers.add(new User("david.johnson", 40, 540, "Castle"));
+    }
+
+    @Test
+    public void testCalculateAverageNumberOfPostsForUsersOlderOrEqualToGivenAge() {
+        // when
+        double averageNumberOfPosts = forumStats.calculateAverageNumberOfPostsForUsersOlderOrEqualToGivenAge(testUsers);
+        // then
+        Assertions.assertEquals(204, averageNumberOfPosts, 0.001);
+    }
+}
