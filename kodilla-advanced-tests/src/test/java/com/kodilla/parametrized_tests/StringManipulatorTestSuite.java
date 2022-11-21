36a760b1d1300632313c0ca46d2362d2df85f5df
Karol Rosiński <krosinski94@gmail.com>
2022-11-21 16:57:21 +0100

    Obiekty jako dane wejściowe p2

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringManipulatorTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringManipulatorTestSuite.java
@@ -1,26 +1,13 @@
 package com.kodilla.parametrized_tests;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StringManipulatorTestSuite {
     private StringManipulator stringManipulator = new StringManipulator();
-
-    private static Stream<Arguments> shouldCalculateStringLengthWithoutSpaces() {
-        return Stream.of(
-                Arguments.of("test", 4),
-                Arguments.of("OtHEr ", 5),
-                Arguments.of("E V e n t", 5),
-                Arguments.of("null ", 4),
-                Arguments.of("A", 1)
-        );
-    }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/stringWithLowerCase.csv", numLinesToSkip = 1)
@@ -29,7 +16,7 @@ class StringManipulatorTestSuite {
     }
 
     @ParameterizedTest
-    @MethodSource
+    @MethodSource(value = "com.kodilla.parametrized_tests.StringSources#provideStringsForTestingLength")
     public void shouldCalculateStringLengthWithoutSpaces(String input, int expected) {
         assertEquals(expected, stringManipulator.getStringLengthWithoutSpaces(input));
     }
