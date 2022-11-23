83aea24db0070b4336c4e678e045f139a82e8d39
Karol Rosiński <krosinski94@gmail.com>
2022-11-23 13:52:10 +0100

    Zadanie: maszyna do gier refactor tests

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTest.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTest.java
@@ -22,15 +22,14 @@ class GamblingMachineTest {
     }
 
     @ParameterizedTest
-    @CsvFileSource(resources = "/validGamblingMachineNumbers.csv", numLinesToSkip = 1)
-    public void shouldCorrectlyValidateNumbers(int validNumber1, int validNumber2, int validNumber3, int validNumber4, int validNumber5, int validNumber6) {
+    @CsvFileSource(resources = "/validGamblingMachineNumbers.csv", numLinesToSkip = 1, delimiter = ':')
+    public void shouldCorrectlyValidateNumbers(String stringNumbers) {
         // given
-        numbers.add(validNumber1);
-        numbers.add(validNumber2);
-        numbers.add(validNumber3);
-        numbers.add(validNumber4);
-        numbers.add(validNumber5);
-        numbers.add(validNumber6);
+        String[] strings = stringNumbers.split(",");
+
+        for(String el : strings) {
+            numbers.add(Integer.valueOf(el));
+        }
         // when
         // then
         assertDoesNotThrow(() -> gamblingMachine.howManyWins(numbers));
@@ -38,14 +37,13 @@ class GamblingMachineTest {
 
     @ParameterizedTest
     @CsvFileSource(resources = "/invalidGamblingMachineNumbers.csv", numLinesToSkip = 1)
-    public void testHowManyWins_withException(int invalidNumber1, int invalidNumber2, int invalidNumber3, int invalidNumber4, int invalidNumber5, int invalidNumber6) {
+    public void testHowManyWins_withException(String stringInvalidNumbers) {
         // given
-        numbers.add(invalidNumber1);
-        numbers.add(invalidNumber2);
-        numbers.add(invalidNumber3);
-        numbers.add(invalidNumber4);
-        numbers.add(invalidNumber5);
-        numbers.add(invalidNumber6);
+        String[] strings = stringInvalidNumbers.split(",");
+
+        for(String el : strings) {
+            numbers.add(Integer.valueOf(el));
+        }
         // when
         // then
         assertThrows(InvalidNumbersException.class, () -> gamblingMachine.howManyWins(numbers));
