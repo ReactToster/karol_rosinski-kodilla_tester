f0a7e87b9c1733dacbdb3a261eeb8cc9ab9fa5a6
Karol Rosiński <krosinski94@gmail.com>
2023-01-23 16:54:16 +0100

    Narzędzia do analizy kodu

---
+++ kodilla-code-coverage/src/main/java/com/kodilla/jacoco/Year.java
@@ -0,0 +1,21 @@
+package com.kodilla.jacoco;
+
+public class Year {
+    int year;
+
+    public Year(int year) {
+        this.year = year;
+    }
+
+    public boolean isLeap() {
+        if (this.year % 4 == 0) {
+            if (this.year % 100 == 0) {
+                return this.year % 400 == 0;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+}
---
+++ kodilla-code-coverage/src/main/java/com/kodilla/spotbugs/BadCodeExample.java
@@ -0,0 +1,25 @@
+package com.kodilla.spotbugs;
+
+import java.util.Objects;
+
+public class BadCodeExample {
+    private String type;
+
+    public static double getPi() {
+        return 3.14;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BadCodeExample badCodeExample = (BadCodeExample) o;
+
+        return Objects.equals(type, badCodeExample.type);
+    }
+}
